253d998da8e429db3db6df85ccaeae4b362ef8e6
henryzhongsc <hzproduction@gmail.com>
2024-10-04 14:35:43 -0400

    🐛 Bugfix mamba2 support to be consistent with benchmarks/benchmark_generation_mamba_simple.py with default sampling params.

--- pipeline/mamba2/eval_longbench.py
+++ pipeline/mamba2/eval_longbench.py
@@ -15,6 +15,7 @@ from pipeline.model_utils import build_chat
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from eval.longbench_utils.eval_long_bench import load_data
 
+
 def get_pred(model, tokenizer, data, device, pipeline_params, eval_params):
     preds = []
     for json_obj in tqdm(data):
@@ -23,25 +24,16 @@ def get_pred(model, tokenizer, data, device, pipeline_params, eval_params):
         # truncate to fit max_length (we suggest truncate in the middle, since the left and right side may contain crucial instructions)
         tokenized_prompt = tokenizer(prompt, truncation=False, return_tensors="pt").input_ids[0]
         if 'truncation_mode' in pipeline_params and pipeline_params['truncation_mode'] == 'middle' and len(tokenized_prompt) > pipeline_params['model_max_len']:
-            half = int(pipeline_params['model_max_len']/2)    
+            half = int(pipeline_params['model_max_len']/2)
             prompt = tokenizer.decode(tokenized_prompt[:half], skip_special_tokens=True)+tokenizer.decode(tokenized_prompt[-half:], skip_special_tokens=True)
 
         if eval_params['dataset'] not in ["trec", "triviaqa", "samsum", "lsht", "lcc", "repobench-p"]: # chat models are better off without build prompts on these tasks
             prompt = build_chat(tokenizer, prompt, pipeline_params['chat_template'])
 
-
         input = tokenizer(prompt, truncation=False, return_tensors="pt").to(device)
         context_length = input.input_ids.shape[-1]
-        kwargs = {
-            "eos_token_id" : [tokenizer.eos_token_id]
-        }
-        if eval_params['dataset'] == "samsum": # prevent illegal output on samsum (model endlessly repeat "\nDialogue"), might be a prompting issue
-            kwargs.update({
-                "min_length" : context_length+1,
-                "eos_token_id" : [tokenizer.eos_token_id, tokenizer.encode("\n", add_special_tokens=False)[-1]]
-            })
-            
-        pred = inference.batch_generate(input.input_ids, model, tokenizer, eval_params['max_new_tokens'], pipeline_params, **kwargs)[0]
+
+        pred = inference.batch_generate(input.input_ids, model, tokenizer, eval_params['max_new_tokens'])[0]
         preds.append({"pred": pred, "answers": json_obj["answers"], "all_classes": json_obj["all_classes"], "length": json_obj["length"]})
 
     return preds
@@ -53,7 +45,7 @@ def eval_longbench(config):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     raw_results = []
 
-    model, tokenizer = inference.initialize_model_tokenizer(pipeline_config=config['pipeline_params'])
+    model, tokenizer = inference.initialize_model_tokenizer(pipeline_params=config['pipeline_params'])
     model.eval()
     preds= get_pred(model, tokenizer, data, device, pipeline_params, eval_params)
 
--- pipeline/mamba2/inference.py
+++ pipeline/mamba2/inference.py
@@ -8,24 +8,24 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import MambaConfig, MambaForCausalLM, AutoTokenizer
 from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
 
-def initialize_model_tokenizer(pipeline_config):
-    tokenizer = AutoTokenizer.from_pretrained(pipeline_config['tokenizer_name'])
+def initialize_model_tokenizer(pipeline_params):
     model = MambaLMHeadModel.from_pretrained(
-        pretrained_model_name=pipeline_config['model_name'],
+        pretrained_model_name=pipeline_params['model_name'],
         dtype=torch.float16,
         device="cuda",
     )
+    tokenizer = AutoTokenizer.from_pretrained(pipeline_params['tokenizer_name'])
+    tokenizer.pad_token = tokenizer.eos_token
+    logger.info(f'Model {model} and Tokenizer {tokenizer} initialized.')
     return model, tokenizer
 
 
 
-def batch_generate(batched_input, model, tokenizer, max_new_tokens, pipeline_config, **kwargs):
+def batch_generate(batched_input, model, tokenizer, max_new_tokens):
     model.eval()
 
     if isinstance(batched_input[0], str):
-        model_inputs = tokenizer(
-            batched_input, return_tensors="pt", padding=True
-        ).to("cuda")
+        model_inputs = tokenizer(batched_input, return_tensors="pt", padding=True).to("cuda")
         input_length = model_inputs.input_ids.shape[1]
         inputs = model_inputs.input_ids
     elif isinstance(batched_input, torch.Tensor):
@@ -35,8 +35,9 @@ def batch_generate(batched_input, model, tokenizer, max_new_tokens, pipeline_con
         logger.error(f"Unknown batched_input:{batched_input}")
         raise ValueError
 
-
-    generated_ids = model.generate(inputs, top_p = 0, temperature = 1, max_length=max_new_tokens, **kwargs)
+    generated_ids = model.generate(inputs, max_length=input_length+max_new_tokens)
     responses = tokenizer.batch_decode(generated_ids[:, input_length:], skip_special_tokens=True)
+
     torch.cuda.empty_cache()
+
     return responses
